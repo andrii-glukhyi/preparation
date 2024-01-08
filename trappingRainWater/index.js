019a66bcde96afd39e08faeccdeb26dd7492e61c
Andrii Glukhyi <a_glukhyi@apple.com>
2024-01-08 11:30:49 -0600

    Added task

--- trappingRainWater/index.js
+++ trappingRainWater/index.js
@@ -2,6 +2,7 @@
  * @param {number[]} height
  * @return {number}
  */
+// SQ: O(n)
 var trap = function(height) {
     let ans = 0;
     if(height.length === 0) {
@@ -22,3 +23,35 @@ var trap = function(height) {
     }
     return ans;
 }
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+//  SQ: O(1)
+var trap = function(height) {
+    let l = 0;
+    let r = height.length - 1;
+    let lmax = 0;
+    let rmax = 0;
+    let ans = 0;
+    while( l < r ) {
+        console.log(l, r)
+        if(height[l] <  height[r]) {
+            if(height[l] > lmax) {
+                lmax = height[l];
+            } else {
+                ans += lmax - height[l];
+            }
+            l++;
+        } else {
+            if(height[r] > rmax) {
+                rmax = height[r];
+            } else {
+                ans += rmax - height[r];
+            }
+            r--
+        }
+    }
+    return ans;
+};
