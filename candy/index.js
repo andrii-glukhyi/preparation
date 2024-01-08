bfe32332bdce055b50e2a681dca993b0ba1fa525
Andrii Glukhyi <a_glukhyi@apple.com>
2024-01-08 11:59:55 -0600

    Added task

---
+++ candy/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    let candies = new Array(ratings.length).fill(1);
+    for(let i = 1; i < ratings.length; i++) {
+        if(ratings[i] > ratings[i-1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+    for(let i = ratings.length - 2; i >= 0; i--) {
+        if(ratings[i] > ratings[i+1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+    let total = 0;
+    for(let n of candies) {
+        total += n
+    }
+    return total
+};
