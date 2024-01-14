b14e254bdc4822b867f267b82740a10d8da96442
Andrii Glukhyi <a_glukhyi@apple.com>
2024-01-14 14:59:45 -0600

    Added task

---
+++ constructBinaryTreeFromPostOrderAndInOrder/index.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if(!preorder || !inorder || preorder.length === 0 || inorder.length === 0) {
+        return null;
+    }
+    let rootValue = preorder[0];
+    let root = new TreeNode(rootValue);
+    let rootIndex = inorder.indexOf(rootValue);
+    root.left = buildTree(preorder.slice(1, rootIndex + 1), inorder.slice(0, rootIndex));
+    root.right = buildTree(preorder.slice(rootIndex + 1), inorder.slice(rootIndex + 1));
+    return root;
+};
