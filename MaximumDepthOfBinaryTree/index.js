f1807ba40a73c4c6d7857a1cb86c4a7290c479e9
Andrii Glukhyi <a_glukhyi@apple.com>
2024-01-14 12:25:12 -0600

    Added task

---
+++ MaximumDepthOfBinaryTree/index.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    return dfs(root)
+};
+
+var dfs = function(node) {
+    if(!node) {
+        return 0;
+    }
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+    return 1 + Math.max(left, right);
+}
