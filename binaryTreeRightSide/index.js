9b59bfb5aaa9d8ed2c03695b8372f38e969d99f3
Andrii Glukhyi <a_glukhyi@apple.com>
2024-01-15 15:26:49 -0600

    Added task

---
+++ binaryTreeRightSide/index.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    let ans = [];
+    if(!root) {
+        return ans;
+    }
+    let queue = [root];
+    ans.push(root.val);
+    while(queue.length > 0) {
+        let nextqueue = [];
+        for(let node of queue) {
+            if(node.left) {
+                nextqueue.push(node.left);
+            }
+            if(node.right) {
+                nextqueue.push(node.right);
+            }
+        }
+        if(nextqueue.length > 0) {
+            ans.push(nextqueue[nextqueue.length - 1].val);
+        }
+        queue = nextqueue;
+    }
+    return ans
+};
